541d46b646d61d4b57c53cb890a6fcfae55da9b0
ryan-j-steele <41901227+ryan-j-steele@users.noreply.github.com>
2018-07-30 12:19:08 +0100

    First test build of PDF text extraction using iTextSharp

---
+++ PDFTextExtractor/PDFTextExtractor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace PDFTextExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(ExtractTextFromPdf(@"D:\Utils\TestData\Files\Small.pdf"));
+            Console.ReadKey();
+        }
+
+
+        public static string ExtractTextFromPdf(string path)
+        {
+            using (PdfReader reader = new PdfReader(path))
+            {
+                StringBuilder text = new StringBuilder();
+
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                }
+
+                return text.ToString();
+            }
+        }
+    }
+}
